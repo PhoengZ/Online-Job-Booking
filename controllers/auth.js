959e94af0789f6b2be4c3ed715cde8b56e7deb3b
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-08-28 23:05:50 +0700

    sending email together

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,18 +4,14 @@ exports.sendEmail = async (users, company) => {
     try {
         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-        for (const user of users) {
-            const msg = {
-                to: user.email,
-                from: "6630199021@student.chula.ac.th",
-                subject: `บริษัท ${company} ที่คุณสนใจได้มีที่ว่างแล้ว`,
-                text: `คุณ ${user.name} รีบไปสมัครสิ!`,
-                html: `<strong>คุณ ${user.name} รีบไปสมัครงานที่บริษัท ${company} สิ!</strong>`,
-            };
-
-            await sgMail.send(msg);
-            console.log(`Email sent successfully to ${user.email}`);
-        }
+        const msg = users.map(user=>({
+            to: user.email,
+            from: "6630199021@student.chula.ac.th",
+            subject: `บริษัท ${company} ที่คุณสนใจได้มีที่ว่างแล้ว`,
+            text: `คุณ ${user.name} รีบไปสมัครสิ!`,
+            html: `<strong>คุณ ${user.name} รีบไปสมัครงานที่บริษัท ${company} สิ!</strong>`,
+        }));
+        await sgMail.send(msg);
 
         return {
             success: true,
@@ -23,11 +19,7 @@ exports.sendEmail = async (users, company) => {
         };
 
     } catch (err) {
-        // Log error ที่ละเอียดขึ้นจาก SendGrid
         console.error('Error sending email:', err);
-        if (err.response) {
-            console.error(err.response.body);
-        }
         return {
             success: false,
             message: err.message
