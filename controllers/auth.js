5b4232f36273ffaa5b7740975062a688655aec2e
Peeradon <peeradonthienpongkasem@gmail.com>
2025-09-30 00:15:03 +0700

    add logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,6 +44,22 @@ exports.login = async (req,res,next) => {
 
 };
 
+
+//@desc    Logout User
+//@route   POST /api/v1/auth/logout
+//@access  Public
+exports.logout = async (req,res,next) => {
+    res.cookie('token', 'none', {
+    expires: new Date(Date.now()), 
+    httpOnly: true
+  });
+
+  res.status(200).json({
+    success: true,
+    message: 'User logged out successfully'
+  });
+};
+
 //Get token from model, create cookie and send response
 const sendTokenResponse = (user , statusCode , res) => {
     //Create Token
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {register,login,getMe} = require('../controllers/auth');
+const {register,login,logout,getMe} = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/getMe',protect,getMe);
 router.post('/login',login);
 router.post('/register',register);
+router.post('/logout',logout);
 
 module.exports = router;
